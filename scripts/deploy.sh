03e646ab5a1eed7661a909302168de2fb816e69c
Aiven <aiven.timptner@icloud.com>
2020-08-11 19:13:37 +0200

    secure ssh_known_hosts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,10 +9,8 @@ git pull
 . venv/bin/activate
 pip install -r requirements.txt
 python manage.py collectstatic --clear --no-input
-python manage.py test
 python manage.py check --deploy
 deactivate
 echo "$PASSWORD" | sudo -S systemctl restart gunicorn
 systemctl status gunicorn
 EOF
-echo ">>>> Finished deploy process <<<<"
