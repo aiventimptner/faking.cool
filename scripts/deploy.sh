9908beee440195e43fd4c4daa393604744048071
Aiven <aiven.timptner@icloud.com>
2020-08-13 00:31:24 +0200

    extend console logs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,13 +5,19 @@ set -e
 echo ">>>> Connect to remote location <<<<"
 ssh -p "$PORT" "$USER@$HOST" << EOF
 cd $DIR
+echo ">>>> Update project files <<<<"
 git pull
 . venv/bin/activate
+echo ">>>> Install packages <<<<"
 pip install -r requirements.txt
-python manage.py collectstatic --clear --no-input
+echo ">>>> Check production environment <<<<"
 python manage.py check --deploy
+echo ">>>> Recollect statics <<<<"
+python manage.py collectstatic --clear --no-input
+echo ">>>> Migrate to database <<<<"
 python manage.py migrate
 deactivate
+echo ">>>> Restart gunicorn service <<<<"
 echo "$PASSWORD" | sudo -S systemctl restart gunicorn
 systemctl status gunicorn
 EOF
