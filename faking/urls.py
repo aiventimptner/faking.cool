3ba3eaf935f153f3ed2676df0de8a9766e9b8dff
aiventimptner <aiven.timptner@icloud.com>
2020-09-21 23:07:10 +0200

    Include base url

--- faking/urls.py
+++ faking/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('/mentoring/')),
     path('mentoring/', include('mentoring.urls')),
     path('admin/', admin.site.urls),
 ]
