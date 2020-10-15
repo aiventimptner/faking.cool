0e6462366b09a5094f4ea48771f3765b8692be5d
Aiven Timptner <aiven.timptner@icloud.com>
2020-10-15 16:58:46 +0200

    Allow generation of missing nicknames

--- mentoring/admin.py
+++ mentoring/admin.py
@@ -1,8 +1,9 @@
 import csv
 
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.http import HttpResponse
 
+from .forms import generate_unique_pseudonym
 from .models import Faculty, Program, Mentor, Mentee
 
 
@@ -24,7 +25,7 @@ class MentorAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'qualification', 'supervision', 'created']
     list_filter = ['faculty', 'program']
     ordering = ['first_name', 'last_name']
-    actions = ['export_as_csv']
+    actions = ['export_as_csv', 'generate_nickname']
 
     def export_as_csv(self, request, queryset):
         response = HttpResponse(content_type='text/csv')
@@ -40,8 +41,19 @@ class MentorAdmin(admin.ModelAdmin):
             writer.writerow([getattr(obj, field) for field in field_names])
 
         return response
-
     export_as_csv.short_description = "Auswahl als CSV exportieren"
+
+    def generate_nickname(self, request, queryset):
+        counter = 0
+        for mentor in queryset:
+            if mentor.nick:
+                continue
+            mentor.nick = generate_unique_pseudonym(mentor)
+            mentor.save()
+            counter += 1
+
+        self.message_user(request, f"Es wurden {counter} Mentor*innen aktualisiert.", messages.SUCCESS)
+    generate_nickname.short_description = "Zufällig Nicknamen generieren"
 
 
 @admin.register(Mentee)
