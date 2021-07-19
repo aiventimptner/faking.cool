18a004d22369e0ec4326ead3335440295f379160
Aiven <aiven@timptner.de>
2021-07-19 21:56:02 +0200

    Add landing page

--- faking/urls.py
+++ faking/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from django.shortcuts import render
 from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda request: render(request, 'faking/index.html')),
     path('admin/', admin.site.urls),
 ]
