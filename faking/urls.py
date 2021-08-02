76412ae1d976fbe4b01013ba4464d084a0eb4290
Aiven <aiven@timptner.de>
2021-08-02 23:03:14 +0200

    Update http error pages

--- faking/urls.py
+++ faking/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.shortcuts import render
 from django.urls import path, include
 from django.shortcuts import render
 
 urlpatterns = [
-    path('', lambda request: render(request, 'faking/index.html')),
+    path('', lambda request: render(request, 'faking/landing_page.html'), name='landing_page'),
     path('mentoring/', include('mentoring.urls')),
     path('votes/', include('votes.urls')),
     path('admin/', admin.site.urls),
