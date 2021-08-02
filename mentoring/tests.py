b3571f244ee5d51da37304bcd771ebd6203ee115
Aiven <aiven@timptner.de>
2021-08-02 23:42:52 +0200

    Update tests.py

--- mentoring/tests.py
+++ mentoring/tests.py
@@ -1,19 +1,3 @@
-from datetime import datetime
 from django.test import TestCase
 
-from .models import Faculty
-
-
-class FacultyModelTest(TestCase):
-
-    def test_color_as_hex(self):
-        """
-        color_as_hex() returns valid hex-color
-        """
-        faculty = Faculty(
-            name='Maschinenbau',
-            slug='fmb',
-            color='ABC123',
-            deadline=datetime.now(),
-        )
-        self.assertEqual(faculty.color_as_hex(), '#ABC123')
+# Create your tests here.
